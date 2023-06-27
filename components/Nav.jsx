b0a3c02e970b88990eaa50379b17289fe03d701a
Mangesh Pralhad Pandit <86043310+alicehack2020@users.noreply.github.com>
2023-06-27 15:02:10 +0530

    sample components addded

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth";
+import logo from "../public/assets/images/logo.svg";
 const Nav = () => {
   return (
-    <div>
-      <h1 className="text-black">Nav</h1>
-    </div>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
+        <Image src={logo} className="object-contain" width={30} height={30}/>
+      <p className="invisible md:visible">Promptopia</p>
+      </Link>
+    </nav>
   );
 };
 
